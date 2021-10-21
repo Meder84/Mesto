3e4d05721cce42b119ee8d357b45dc5aabde2fb6
Meder <medi_medi@mail.ru>
2021-10-21 11:21:37 +0300

    feat: сделать popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,46 @@
     /* функциональность popup  */
-const popupElement = document.querySelector('.popup');
-const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
+const popup = document.querySelector('.popup');
+const popupCloseButtonElement = popup.querySelector('.popup__close-button');
 const popupOpenButtonElement =  document.querySelector('.profile__edit-button');
-
-const closePopupByClickOverlay = function(event) {
-  console.log(event.target, event.currentTarget);
-  if(event.target === event.currentTarget) {
-    closePopup();
-  }
-};
+const popupSubmitButton = popup.querySelector('.popup__submit-button')
 
 const openPopup = function() {
-  popupElement.classList.add('popup_opened');
+  popup.classList.add('popup_opened');
 };
 
 const closePopup = function() {
-  popupElement.classList.remove('popup_opened');
+  popup.classList.remove('popup_opened');
 };
 
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
-popupElement.addEventListener('click', closePopupByClickOverlay);
+popupSubmitButton.addEventListener('click', closePopup);
 
 
   /*  Функциональность элемента редактирование  */
-const nameInput = popupElement.querySelector('.popup__name-input');
-const jobInput = popupElement.querySelector('.popup__job-input');
-const submitButton = popupElement.querySelector('.popup__submit-button');
+const nameInput = popup.querySelector('.popup__name-input');
+const jobInput = popup.querySelector('.popup__job-input');
 
-const profiel = document.querySelector('.profiel')
-const profileName = profiel.querySelector('profile__name')
-const profileJob = profiel.querySelector('profile__job')
+const profile = document.querySelector('.profile');
+const profileName = profile.querySelector('.profile__name');
+const profileJob = profile.querySelector('.profile__job');
 
-const editUser = function() {
-  
+const editUserName = function() {
+  if(nameInput.value === '' || nameInput.value === Number) {
+    return;
+  } else {
+    profileName.textContent = nameInput.value;
+  }
 }
+
+const editUserJob = function() {
+  if(jobInput.value === '' || jobInput.value === Number) {
+    return;
+  } else {
+    profileJob.textContent = jobInput.value;
+  }
+}
+
+popupSubmitButton.addEventListener('click', editUserName);
+popupSubmitButton.addEventListener('click', editUserJob);
+
